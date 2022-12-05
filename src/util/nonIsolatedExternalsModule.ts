8db7536e388fa6dfdfd5ff53db3f35238ba7390d
Steve Matney <steve.matney@extendhealth.com>
2022-12-05 15:33:53 -0700

    Fix: ensuring nonIsolatedModule works in non-window environments

--- src/util/nonIsolatedExternalsModule.ts
+++ src/util/nonIsolatedExternalsModule.ts
@@ -3,17 +3,20 @@ import getValue from './getValue';
 
 export {};
 
-const windowProxy = new Proxy(window || global || self, {
-  get(...args): unknown {
-    const [, prop] = args;
-    if (typeof prop === 'string' && prop.includes('.')) {
-      return getValue(
-        prop,
-        (windowProxy as unknown) as Record<string, unknown>
-      );
-    }
-    return Reflect.get(...args) as unknown;
-  },
-});
+const windowProxy = new Proxy(
+  globalThis?.window || globalThis || global || self,
+  {
+    get(...args): unknown {
+      const [, prop] = args;
+      if (typeof prop === 'string' && prop.includes('.')) {
+        return getValue(
+          prop,
+          (windowProxy as unknown) as Record<string, unknown>
+        );
+      }
+      return Reflect.get(...args) as unknown;
+    },
+  }
+);
 
 __webpack_modules__[EXTERNALS_MODULE_NAME] = windowProxy;
