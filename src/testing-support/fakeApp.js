2bc7b8bcc3f36d44136f1f6750ac3267b1b2dbe3
Steve Matney <steve.matney@extendhealth.com>
2023-06-09 11:44:42 -0600

    New: initial pass at unpromised externals working around webpack/webpack#16544

--- src/testing-support/fakeApp.js
+++ src/testing-support/fakeApp.js
@@ -1,8 +1,10 @@
-/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 
 export default function fake() {
   const answer = 1 + 1;
   return answer;
 }
+
+ReactDOM.render(React.createElement(Modal), document.getElementById('root'));
